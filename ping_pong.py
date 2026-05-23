7ced7644ebdf300557738989b11d04fcfa0a2232
Ektwr131 <ektwr2013@gmail.com>
2026-05-23 11:25:36 +0300

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -4,6 +4,7 @@ init()
 
 window = display.set_mode((700, 500))
 display.set_caption('Ping Pong')
+ball = transform.scale(image.load("ball.png"), (100, 75))
 
 game = True
 
@@ -18,6 +19,7 @@ while game:
             game = False
 
     window.fill(background_color)
-
+    window.blit(ball, (300, 230))
+    
     game_clock.tick(FPS)
     display.update()
